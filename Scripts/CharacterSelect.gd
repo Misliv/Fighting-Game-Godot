a0da2bd80b473b629560a769e87aec5d96419a8c
Misliv <mayandmays@hotmail.com>
2024-01-24 12:24:08 +0100

    Added a background to the main menu
    Added a simple button that goes from character screen to the game.
    
    Added animated idle sprite

---
+++ Scripts/CharacterSelect.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+func _on_characterselect_pressed():
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Scenes/control.tscn")
